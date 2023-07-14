db50ab4e85c4a129e7b4011bd5ab02163b67323e
Zac Jones <zacjones93@gmail.com>
2023-07-14 10:39:36 -0600

    update state to track score added

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,16 +1,23 @@
 import { useState, useReducer, useEffect } from "react";
-
+import toast, { Toaster } from "react-hot-toast";
+import { twMerge } from "tailwind-merge";
+import cx from "classnames";
 type GameState = "Set up" | "inProgress" | "last round" | "complete";
+
+/*
+!TODO:
+  - refactor to store score added each round, resulting score as derived state
+  - edit each round score
+*/
 
 const gameObject = {
 	state: "inProgress",
 	winner: "",
 	scores: {
-    "Zac": [0],
-    "Mariah": [0],
-    "Chrisy": [0],
-    "Bill": [0],
-    
+    zac: [0],
+    mariah: [0],
+    bill: [0],
+    chrisy: [0],
   },
 };
 
@@ -22,11 +29,16 @@ let initialInputState = Object.keys(gameObject.scores).reduce(
 	{}
 );
 
+let DetailIcon = ({className}: {className: string}) => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.0} stroke="currentColor" className={twMerge("w-4 h-4", className)}>
+      <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9zm3.75 11.625a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
+    </svg>
+)
+
 const Game = () => {
 	const [game, setGame] = useReducer((state: any, newState: any) => {
 		switch (newState.type) {
 			case "startGame":
-				console.log("newState", newState);
 
 				let scores = newState.setUpFormValues
 					.filter((player: any) => !!player?.name)
@@ -40,18 +52,25 @@ const Game = () => {
 				let playersToCheck = Object.keys(newState.scores);
 
 				let newScores = playersToCheck.map((player: any) => {
-					let currentScore = state.scores[player].slice(-1).pop();
+					let currentScore = state.scores[player].slice(-1).pop()
+          let newScore = newState.scores[player].slice(-1).pop()
+
+          if (newScore % 50 !== 0) {
+            toast.error("Score must be a multiple of 50")
+            return { ...state.scores }
+          };
+
 					if (
 						currentScore < 600 &&
-						newState.scores[player].slice(-1).pop() < 600
-					)
-						return { ...state.scores };
+						newScore < 600
+					){
+            toast.error("Initial score must be at least 600")
+						return { ...state.scores };}
 
 					return { ...state.scores, ...newState.scores };
 				});
 
 				return { ...state, scores: newScores.pop() };
-
 			default:
 				return state;
 		}
@@ -61,7 +80,29 @@ const Game = () => {
 		initialInputState
 	);
 
-	const gameScores = game.scores;
+	const gameScores = game?.scores;
+  const displayScores = Object.keys(gameScores).reduce((prevPlayers: any, currPlayer: any) => {
+    let tempAggregator = 0
+    let newScores = gameScores[currPlayer].map((score: number, i: number, array: number[]) => {
+      if(i === 0) return score
+
+      tempAggregator += array[i - 1]
+
+      return score + tempAggregator
+    })
+
+    return {
+      ...prevPlayers,
+      [currPlayer]: newScores 
+    }
+  }, {})
+  console.log({displayScores})
+  const currentPlayerScores: any = Object.keys(displayScores).reduce((prev, curr) => {
+    return {
+      ...prev,
+      [curr]: displayScores[curr].slice(-1).pop()
+    }
+  }, {})
 	const players = Object.keys(game.scores);
 	// useEffect(() => {
 	//   console.log("game scores", gameScores)
@@ -82,8 +123,8 @@ const Game = () => {
 
 		newScoresFromRound.forEach((player: any) => {
 			let currentScoreArray = game.scores[player];
-			let newScore =
-				parseInt(currentScoreArray.slice(-1)) + parseInt(gameInput[player]);
+			let newScore = parseInt(gameInput[player])
+				
 
 			setGameInput({ [player]: "" });
 			setGame({
@@ -92,8 +133,6 @@ const Game = () => {
 					[player]: [...currentScoreArray, newScore],
 				},
 			});
-
-			console.log({ gameInput }, { [player]: 0 });
 		});
 	};
 
@@ -112,9 +151,9 @@ const Game = () => {
 		setGame({ type: "startGame", setUpFormValues });
 	};
 
-	let handleSetUpChange = (i, e) => {
-		let newFormValues = [...setUpFormValues];
-		newFormValues[i][e.target.name] = e.target.value;
+	let handleSetUpChange = (i: any, e: any) => {
+		let newFormValues: any = [...setUpFormValues];
+		newFormValues[i][e.target.name] = e.target.value
 		setSetUpFormValues(newFormValues);
 	};
 
@@ -122,14 +161,15 @@ const Game = () => {
 		setSetUpFormValues([...setUpFormValues, { name: "" }]);
 	};
 
-	let removeSetUpFormFields = (i) => {
+	let removeSetUpFormFields = (i: any) => {
 		let newFormValues = [...setUpFormValues];
 		newFormValues.splice(i, 1);
 		setSetUpFormValues(newFormValues);
 	};
 
 	return (
-		<div className="flex self-center justify-between h-full w-full gap-4 text-black">
+		<div className="flex flex-wrap sm:flex-nowrap self-center justify-between h-full w-full gap-4 text-black">
+      <Toaster />
 			{game.state === "Set up" && (
 				<div className="flex flex-col justify-between h-full gap-4">
 					<h2 className="text-2xl">Add how many players you'd like to keep track of 👇</h2>
@@ -177,24 +217,52 @@ const Game = () => {
 			{game.state === "inProgress" &&
 				Object.keys(game.scores).map((playerScore: any) => {
 					return (
-						<div className="h-full w-full" key={playerScore}>
-							<div className="mb-8 @container w-full">
-                <div className="flex @xs:flex-wrap gap-2 border-b-2 w-full">
-								<h2 className="text-xl shrink-0 self-center">{playerScore}</h2>
-								<form onSubmit={handleSubmit} id={`${playerScore}-form`} className="w-full">
+						<div className="h-full w-full max-w-sm" key={playerScore}>
+							<div className="mb-2 sm:mb-8 w-full">
+                <div className="gap-2 border-b-2 w-full">
+                <div className="flex justify-between">
+                  <div className="flex gap-2">
+                    <h2 className="text-3xl sm:text-xl text-center">{playerScore}</h2>
+                      <div className="relative self-center group cursor-pointer sm:cursor-default ">
+                        <DetailIcon className=" sm:hidden h-5 w-5"/>
+                        <div className=" left-12 opacity-0 group-hover:opacity-100 select-none sm:group-hover:opacity-0 duration-300 absolute inset-0 z-10 flex justify-center items-center text-xs text-black font-semibold bg-opacity-50 rounded bg-white">score details</div>
+                      </div>
+                  </div>
+                  <p className="text-3xl sm:text-lg text-center md:hidden">{currentPlayerScores[playerScore]}</p>
+                </div>
+								<form onSubmit={handleSubmit} id={`${playerScore}-form`} className="mt-4 w-full">
 									<input
 										className="w-full py-2 bg-gray-50 text-black rounded px-4 "
 										placeholder={`enter score...`}
 										id={playerScore}
 										onChange={handleChange}
-										value={gameInput[playerScore]}
+										value={gameInput[playerScore] || ""}
 										type="number"
+                    step={50}
 									/>
 								</form>
                 </div>
-								<ul className="space-y-4 ">
-									{game.scores[playerScore].map((score: any) => {
-										return <li className="text-xl">{score}</li>;
+								<ul className="flex-col md:flex hidden">
+									{game.scores[playerScore].map((score: number, i: number) => {
+                    let currentScore = displayScores[playerScore][i]
+										return (
+                      <li className="text-lg w-fit self-end flex " key={`${playerScore}-${score}-${i}`}>
+                        <div
+                          className={cx(" text-gray-500 font-bold text-xs self-center mr-8", {
+                            "hidden": score === 0,
+                          })}
+                        >+{score}</div>
+                        <div
+                          className={cx(" w-16", {
+                            "hidden": currentScore === 0,
+                          })
+                          }
+                        >
+                          <div className="w-fit float-right">
+                            {currentScore}
+                          </div>
+                        </div>
+                      </li>);
 									})}
 								</ul>
 							</div>
