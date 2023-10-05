2f63f934e6ab804b7e3cd76f4a9e24f45d3106c0
Zac Jones <zacjones93@gmail.com>
2023-10-05 11:40:43 -0600

    remove adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import node from "@astrojs/node";
-import netlify from "@astrojs/netlify/functions";
 
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind(), react()],
-  adapter: vercel()
 });
