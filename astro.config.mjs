ccebdf1c7e55fb06099245dd982fd329e875ca42
Zac Jones <zacjones93@gmail.com>
2023-10-05 11:34:06 -0600

    move to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
-
 import netlify from "@astrojs/netlify/functions";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind(), react()],
-  adapter: netlify()
+  adapter: vercel()
 });
