a69db0993c67defc8ab245b7d1334643e88cc830
Zac Jones <zacjones93@gmail.com>
2023-10-05 11:09:41 -0600

    update build config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from '@astrojs/vercel/serverless';
+import { resolve } from "path";
 
 
 // https://astro.build/config
@@ -9,13 +10,13 @@ export default defineConfig({
   output: 'server',
   integrations: [tailwind(), react()],
   adapter: vercel(),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@tests": path.resolve(__dirname, "./tests")
-    }
-  },
   build:{
-    outDir: "build"
+    outDir: "build",
+    rollupOptions: {
+      external: [
+          "react", // ignore react stuff
+          "react-dom",
+      ],
+  }
   }
 });
